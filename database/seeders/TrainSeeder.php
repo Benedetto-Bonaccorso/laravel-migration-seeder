57438b1d571e9c1c300a4440c78dda1aac19d6c0
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-12-20 18:05:44 +0100

    consegna_e_bonus

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -22,7 +22,7 @@ class TrainSeeder extends Seeder{
             $train->azienda = $faker->randomElement(['aaaaa', 'bbbb', 'ccc', 'dd', 'e']);
             $train->stazione_partenza = $faker->randomElement(['aaaaa', 'bbbb', 'ccc', 'dd', 'e']);
             $train->stazione_arrivo = $faker->randomElement(['aaaaa', 'bbbb', 'ccc', 'dd', 'e']);
-            $train->orario_partenza = $faker->randomElement(['aaaaa', 'bbbb', 'ccc', 'dd', 'e']);
+            $train->orario_partenza = $faker->time();
             $train->orario_arrivo = $faker->numberBetween(2, 10);
             $train->codice_treno = $faker->numberBetween(2, 10);
             $train->numero_carrozze = $faker->numberBetween(2, 9);
